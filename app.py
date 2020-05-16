9ed9a3a3ada6e76fd9963bfd68a8dd5fa84d64e2
Grzegorz Jaworski <grzegorz.jaworski@whiteaster.com>
2020-05-16 16:34:49 +0200

    Add sqlite3 get_db and close_connections functions

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
 # -*- coding: utf-8 -*-
 import os
+import sqlite3
 from ast import literal_eval
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from flask import Flask
+from flask import Flask, g
 
+DATABASE = './db.sqlite3'
 
 server = Flask('Dash')
 server.secret_key = os.environ.get('SECRET_KEY', 'secret123')
 
 app = dash.Dash(__name__, server=server)
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+
+@server.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
 
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
