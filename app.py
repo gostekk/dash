014cb556ca8df6b1c64684db0aad39b9ee187112
mbrodziak <matousb96@gmail.com>
2020-05-23 17:53:44 +0200

    Change type few graphs

--- app.py
+++ app.py
@@ -43,9 +43,9 @@ with server.app_context():
     never = get_data('SELECT * FROM data WHERE OpenSourcer = "Never"')
     open_source = get_data('SELECT OpenSourcer, COUNT(OpenSourcer) FROM data GROUP BY OpenSourcer')
     student = get_data('SELECT Student, COUNT(Student) FROM data WHERE Student != "NA" GROUP BY Student')
-    developer_full_stack = get_data('SELECT * FROM data WHERE DevType LIKE "%Developer, full-stack%"')
-    developer_back_end = get_data('SELECT * FROM data WHERE DevType LIKE "%Developer, back-end%"')
-    developer_front_end = get_data('SELECT * FROM data WHERE DevType LIKE "%Developer, front-end%"')
+    developer_full_stack = get_data('SELECT DevType FROM data WHERE DevType LIKE "%Developer, full-stack%"')
+    developer_back_end = get_data('SELECT DevType FROM data WHERE DevType LIKE "%Developer, back-end%"')
+    developer_front_end = get_data('SELECT DevType FROM data WHERE DevType LIKE "%Developer, front-end%"')
     developer_desktop = get_data(
         'SELECT DevType FROM data WHERE DevType LIKE "%Developer, desktop or enterprise applications%"')
     developer_mobile = get_data('SELECT DevType FROM data WHERE DevType LIKE "%Developer, mobile%"')
@@ -162,8 +162,11 @@ def update_content(category_name):
                     id='hobbyist',
                     figure={
                         'data': [
-                            {'x': [hobbyist[1][0]], 'y': [hobbyist[1][1]], 'type': 'bar', 'name': hobbyist[1][0]},
-                            {'x': [hobbyist[0][0]], 'y': [hobbyist[0][1]], 'type': 'bar', 'name': hobbyist[0][0]},
+                            {
+                                'values': [hobbyist[1][1], hobbyist[0][1]],
+                                'type': 'pie',
+                                'labels': [hobbyist[1][0], hobbyist[0][0]],
+                            },
                         ],
                         'layout': {
                             'title': 'Coding as a Hobby'
@@ -200,9 +203,11 @@ def update_content(category_name):
                     id='student',
                     figure={
                         'data': [
-                            {'x': [student[0][0]], 'y': [student[0][1]], 'type': 'bar', 'name': student[0][0]},
-                            {'x': [student[1][0]], 'y': [student[1][1]], 'type': 'bar', 'name': student[1][0]},
-                            {'x': [student[2][0]], 'y': [student[2][1]], 'type': 'bar', 'name': student[2][0]},
+                            {
+                                'values': [student[0][1], student[1][1], open_source[2][1]],
+                                'type': 'pie',
+                                'labels': [student[0][0], student[1][0], student[2][0]],
+                            },
                         ],
                         'layout': {
                             'title': 'How Many Developers are Students?'
@@ -247,30 +252,19 @@ def update_content(category_name):
                     id='undergraduate_major',
                     figure={
                         'data': [
-                            {'x': ['Computer science...'], 'y': [undergrad_major[6][1]], 'type': 'bar',
-                             'name': undergrad_major[6][0]},
-                            {'x': ['Another engineering...'], 'y': [undergrad_major[5][1]], 'type': 'bar',
-                             'name': undergrad_major[5][0]},
-                            {'x': ['Information systems...'], 'y': [undergrad_major[9][1]], 'type': 'bar',
-                             'name': undergrad_major[9][0]},
-                            {'x': ['Web development...'], 'y': [undergrad_major[11][1]], 'type': 'bar',
-                             'name': undergrad_major[11][0]},
-                            {'x': ['Natural science'], 'y': [undergrad_major[3][1]], 'type': 'bar',
-                             'name': undergrad_major[3][0]},
-                            {'x': ['Mathematics...'], 'y': [undergrad_major[10][1]], 'type': 'bar',
-                             'name': undergrad_major[10][0]},
-                            {'x': ['Business discipline'], 'y': [undergrad_major[0][1]], 'type': 'bar',
-                             'name': undergrad_major[0][0]},
-                            {'x': ['Humanities discipline'], 'y': [undergrad_major[2][1]], 'type': 'bar',
-                             'name': undergrad_major[2][0]},
-                            {'x': ['Social science'], 'y': [undergrad_major[4][1]], 'type': 'bar',
-                             'name': undergrad_major[4][0]},
-                            {'x': ['Fine arts...'], 'y': [undergrad_major[7][1]], 'type': 'bar',
-                             'name': undergrad_major[7][0]},
-                            {'x': ['Any declared a major'], 'y': [undergrad_major[8][1]], 'type': 'bar',
-                             'name': undergrad_major[8][0]},
-                            {'x': ['Health science'], 'y': [undergrad_major[1][1]], 'type': 'bar',
-                             'name': undergrad_major[1][0]},
+                            {
+                                'values': [undergrad_major[6][1], undergrad_major[5][1], undergrad_major[9][1],
+                                           undergrad_major[11][1], undergrad_major[3][1], undergrad_major[10][1],
+                                           undergrad_major[10][1], undergrad_major[0][1], undergrad_major[2][1],
+                                           undergrad_major[4][1], undergrad_major[7][1], undergrad_major[8][1],
+                                           undergrad_major[1][1]],
+                                'type': 'pie',
+                                'labels': [undergrad_major[6][0], undergrad_major[5][0], undergrad_major[9][0],
+                                           undergrad_major[11][0], undergrad_major[3][0], undergrad_major[10][0],
+                                           undergrad_major[10][0], undergrad_major[0][0], undergrad_major[2][0],
+                                           undergrad_major[4][0], undergrad_major[7][0], undergrad_major[8][0],
+                                           undergrad_major[1][0]],
+                            },
                         ],
                         'layout': {
                             'title': 'Undergraduate Major'
@@ -286,18 +280,14 @@ def update_content(category_name):
                     id='years_since_learning_to_code',
                     figure={
                         'data': [
-                            {'x': ['Less then 10 years'], 'y': [years_since_learning_code[0]], 'type': 'bar',
-                             'name': 'Less then 10 years'},
-                            {'x': ['10 to 19 years'], 'y': [years_since_learning_code[1]], 'type': 'bar',
-                             'name': '10 to 19 years'},
-                            {'x': ['20 to 29 years'], 'y': [years_since_learning_code[2]], 'type': 'bar',
-                             'name': '20 to 29 years'},
-                            {'x': ['30 to 39 years'], 'y': [years_since_learning_code[3]], 'type': 'bar',
-                             'name': '30 to 39 years'},
-                            {'x': ['40 to 49 years'], 'y': [years_since_learning_code[4]], 'type': 'bar',
-                             'name': '40 to 49 years'},
-                            {'x': ['50 years or more'], 'y': [years_since_learning_code[5]], 'type': 'bar',
-                             'name': '50 years or more'},
+                            {
+                                'values': [years_since_learning_code[0], years_since_learning_code[1],
+                                           years_since_learning_code[2], years_since_learning_code[3],
+                                           years_since_learning_code[4], years_since_learning_code[5]],
+                                'type': 'pie',
+                                'labels': ['Less then 10 years', '10 to 19 years', '20 to 29 years', '30 to 39 years',
+                                           '40 to 49 years', '50 years or more'],
+                            },
                         ],
                         'layout': {
                             'title': 'Years Since Learning to Code'
@@ -311,18 +301,14 @@ def update_content(category_name):
                     id='years_coding_professionally',
                     figure={
                         'data': [
-                            {'x': ['Less then 10 years'], 'y': [years_coding_professionally[0]], 'type': 'bar',
-                             'name': 'Less then 10 years'},
-                            {'x': ['10 to 19 years'], 'y': [years_coding_professionally[1]], 'type': 'bar',
-                             'name': '10 to 19 years'},
-                            {'x': ['20 to 29 years'], 'y': [years_coding_professionally[2]], 'type': 'bar',
-                             'name': '20 to 29 years'},
-                            {'x': ['30 to 39 years'], 'y': [years_coding_professionally[3]], 'type': 'bar',
-                             'name': '30 to 39 years'},
-                            {'x': ['40 to 49 years'], 'y': [years_coding_professionally[4]], 'type': 'bar',
-                             'name': '40 to 49 years'},
-                            {'x': ['50 years or more'], 'y': [years_coding_professionally[5]], 'type': 'bar',
-                             'name': '50 years or more'},
+                            {
+                                'values': [years_coding_professionally[0], years_coding_professionally[1],
+                                           years_coding_professionally[2], years_coding_professionally[3],
+                                           years_coding_professionally[4], years_coding_professionally[5]],
+                                'type': 'pie',
+                                'labels': ['Less then 10 years', '10 to 19 years', '20 to 29 years', '30 to 39 years',
+                                           '40 to 49 years', '50 years or more'],
+                            },
                         ],
                         'layout': {
                             'title': 'Years Coding Professionally'
